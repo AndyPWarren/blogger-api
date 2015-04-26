5dee2026853313163892cb8ffc9c2f9cb87c23cf
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-04-26 16:46:48 +0100

    initial commit with getOne function

--- api/controllers/SiteController.js
+++ api/controllers/SiteController.js
@@ -1,14 +1,35 @@
 'use strict';
 
 /**
- * A group represents a shared asset which other data can be associated with. \
- * A user must have a membership with a group to enable interaction with associated data.
+ * A site represents a customer website which other data can be associated with. \
+ * A user must have a membership with a site to enable interaction with blogs.
  *
- * API docs: http://docs.firstmate.apiary.io/#reference/group
- * @module GroupController
+ * API docs: http://docs.blogger.apiary.io/#reference/site
+ * @module SiteController
  */
 var SiteController = {
 
+
+    getOne: function (req, res) {
+
+        var reqDomain = req.param("domain");
+
+        Site.findOne()
+            .where({domain: reqDomain})
+            .then(function(site){
+                return [site];
+            })
+            .spread(function(site){
+
+                if (site !== undefined){
+                    return ResponseService.send(req, res, {
+                        data: site
+                    });
+                } else {
+                    return res.notFound(req.__('Response.404'));
+                }
+            });
+    }
 };
 
 /**
