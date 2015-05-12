f4751b60c433650f53facb1b742918cd44b26e69
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-05-12 16:52:26 +0100

    authorize function

--- api/controllers/SiteController.js
+++ api/controllers/SiteController.js
@@ -22,14 +22,68 @@ var SiteController = {
             .spread(function(site){
 
                 if (site !== undefined){
-                    return ResponseService.send(req, res, {
-                        data: site
-                    });
+                    if (site.authorized === false) {
+                        return res.forbidden(req.__('Error.Sites.NotAuthorized'));
+                    } else {
+                        return ResponseService.send(req, res, {
+                            data: site
+                        });
+                    }
                 } else {
                     return res.notFound(req.__('Response.404'));
                 }
             });
-    }
+    },
+
+    getAll: function (req, res) {
+
+        Site.find()
+            .where({authorized: false})
+            .then(function(sites){
+                return [sites];
+            })
+            .spread(function(sites){
+                if (sites === undefined){
+                    var message = "All sites have been authorized";
+                    return ResponseService.send(req, res, {
+                        data: message
+                    });
+                } else {
+                    return ResponseService.send(req, res, {
+                        data: sites
+                    });
+                }
+        });
+    },
+
+    authorize: function (req, res) {
+
+        var reqDomain = req.param('domain');
+
+        var values = {
+            authorized: true
+        };
+
+        Site.findOne()
+            .where({domain: reqDomain})
+            .then(function(site){
+
+                if (site === undefined) {
+                    return res.notFound(req.__('Response.404'));
+                } else {
+
+                    Site.update(site.id, values).exec(function(err, site){
+                        if (err) return res.server(req.__('Response.500'));
+
+                        return ResponseService.send(req, res, {
+                            data: site
+                        });
+                    });
+                }
+            })
+
+    },
+
 };
 
 /**
