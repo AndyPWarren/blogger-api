5dccc160e754dc1606441ba8eda08a66c0616b84
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-05-18 23:55:56 +0100

    copying bower deps based on data in a json file

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -15,14 +15,31 @@
  */
 module.exports = function(grunt) {
 
+    var assets = grunt.file.readJSON("./assets/assets.json").files;
+    var jsAssets = grunt.file.readJSON("./assets/assets.json").js;
+    var cssAssets = grunt.file.readJSON("./assets/assets.json").css;
+//    applicationFiles: grunt.file.readJSON("./scripts.json").application,
+
 	grunt.config.set('copy', {
 		dev: {
 			files: [{
 				expand: true,
 				cwd: './assets',
-				src: ['**/*.!(coffee|less)'],
+				src: assets,
 				dest: '.tmp/public'
-			}]
+			},{
+                expand: true,
+                cwd: './assets',
+                src: jsAssets,
+                dest: '.tmp/public/js/dependencies'
+            },{
+                expand: true,
+                cwd: './assets',
+                src: cssAssets,
+                dest: '.tmp/public/styles',
+                flatten: true
+            }],
+
 		},
 		build: {
 			files: [{
