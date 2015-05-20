1a29176128ac87853a9f916a43dbf631bf4d5e4c
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-05-20 12:17:32 +0100

    improved timeSplit readability

--- assets/js/components/api/timeSplitter.js
+++ assets/js/components/api/timeSplitter.js
@@ -4,24 +4,23 @@ angular.module("bloggerOverlord.api.splitTime", [
     /**
      * Function for converting date/time strings
      * to date(DMY) and time
-     * @param   {String} time input time string
-     * @returns {Array}[0date (DD-MM-YYYY)
-     * @returns {Array}[1time (HH:MM:SS)
+     * @param   {String} time  input time string in with time separated with 'T' and milliseconds with '.'
+     * @returns {Array}[0]date (DD-MM-YYYY)
+     * @returns {Array}[1]time (HH:MM:SS)
      */
-
-    function(time){
-
-
-
-    var timeDateStr = time.split('T'),
-        date = timeDateStr[0],
-        dateStr= date.split('-'),
-        year = dateStr[0],
-        month = dateStr[1],
-        day = dateStr[2],
-        timeStr = timeDateStr[1].split('.'),
-        time = timeStr[0];
-
-    var dateDMY = day + '-' + month + '-' + year;
-    return [dateDMY, time];
-});
+    function(dateTime){
+        // separate date and time by 'T'
+        var dateTimeArr = dateTime.split('T');
+        // take date and separate into Y,M,D
+        var dateArr = dateTimeArr[0].split('-'),
+            year = dateArr[0],
+            month = dateArr[1],
+            day = dateArr[2];
+        // take time and remove milliseconds
+        var timeArr = dateTimeArr[1].split('.'),
+            time = timeArr[0];
+        //construct date format DD-MM-YYYY
+        var dateDMY = day + '-' + month + '-' + year;
+        return [dateDMY, time];
+    }
+);
