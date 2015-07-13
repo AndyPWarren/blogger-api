278516b50726d206c35a0f3e389ae04389087521
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-07-13 16:01:45 +0100

    cleaned up console loggign

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -1,15 +1,11 @@
-
+'use strict';
 
 module.exports = {
 
     showDashboard: function (req, res) {
 
-//        console.log(req.session);
-//        console.log(req.session.me);
-//        console.log(req.session.passport);
-        console.log(req.session);
+
         if(!req.session.authenticated) {
-            console.log("load login");
             return res.view('login');
         } else {
             var user = req.session.passport.user;
