9d0d22cb8ac06788c6972add3cca0484d4469b2c
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-08-11 21:44:42 +0100

    added user  colour and initials

--- api/models/User.js
+++ api/models/User.js
@@ -40,10 +40,16 @@ module.exports = {
         lastName        : { type: 'string', required: true },
         passports       : { collection: 'Passport', via: 'user' },
         admin           : { type: 'boolean', required: true, defaultsTo: false },
+        colour          : { type: 'string'},
+
 
         // Instance methods
         name        : function () {
             return this.firstName + ' ' + this.lastName;
+        },
+
+        initials    : function () {
+            return this.firstName.split('')[0] + this.lastName.split('')[0];
         },
 
         // Overrides the model object
@@ -52,9 +58,22 @@ module.exports = {
             delete user.passports;
 
             user.fullName = this.name();
+            user.initials = this.initials();
 
             return user;
         }
+
+
+    },
+
+    beforeCreate: function (values, cb){
+        var colours = ["#e67373", "#804040","#bf3030","#5c73b8","#554080","#bf6a30"];
+
+        var rnd = Math.floor((Math.random()*5)+1);
+        console.log(rnd);
+        values.colour = colours[rnd];
+        cb();
+
     }
 
 };
